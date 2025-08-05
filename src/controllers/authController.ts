470b9b5226c214e5fae292fb1476c4bd4ed0a73b
Jhonata Souza <nasota.dev@gmail.com>
2025-08-05 20:40:36 -0300

    starting auth controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,10 @@
+import {Request, Response } from "fastify"
 import prisma from "../lib/db.ts"
 
+
 export class AuthController {
-  async register
+  async register(request:Request, response:Response) {
+    const {name, email, cpf, login, pass}
+  }
 }
 
