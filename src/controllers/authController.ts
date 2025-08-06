6fefe55305bf8277bdf1f501645a2418a2564b89
Jhonata Souza <nasota.dev@gmail.com>
2025-08-06 19:45:42 -0300

    saving changes

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,10 +1,13 @@
-import {Request, Response } from "fastify"
+import {Request, Response } from "express"
 import prisma from "../lib/db.ts"
 
 
 export class AuthController {
-  async register(request:Request, response:Response) {
-    const {name, email, cpf, login, pass}
+  async signUp(request:Request, response:Response) {
+    const {name, email, cpf, login, pass} = request.body
+  }
+  async singIn(request:Request, response:Response){
+    const {login, pass} = request.body
   }
 }
 
